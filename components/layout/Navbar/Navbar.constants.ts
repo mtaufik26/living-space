558c1e7b03fa42cdfeef0d785efb2abf6a8ffab4
mtaufik26 <taufikediansyah@gmail.com>
2026-05-05 14:21:18 +0700

    Update Navbar

--- components/layout/Navbar/Navbar.constants.ts
+++ components/layout/Navbar/Navbar.constants.ts
@@ -1,12 +1,12 @@
 export const NAVBAR_LINKS = [
   { label: "Home", href: "/" },
-  { label: "About", href: "/#about" },
-  { label: "Services", href: "/#services" },
-  { label: "Projects", href: "/#projects" },
+  { label: "About", href: "/about" },
+  { label: "Services", href: "/services" },
+  { label: "Projects", href: "/projects" },
   { label: "Testimonials", href: "/#testimonials" },
   { label: "Team", href: "/#team" },
-  { label: "Careers", href: "/#careers" },
-  { label: "Blog", href: "/#blog" },
+  { label: "Careers", href: "/careers" },
+  { label: "Blog", href: "/blog" },
   { label: "FAQ", href: "/#faq" },
 ];
 
