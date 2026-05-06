5e9704bca3b9d81822b07ccfde1582ed35d0e1f1
mtaufik26 <taufikediansyah@gmail.com>
2026-05-06 20:01:22 +0700

    Fix Bugs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/living-space",
   trailingSlash: true,
   images: {
     unoptimized: true,
